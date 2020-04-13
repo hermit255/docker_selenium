42dae4f55ca333eb45d306496d56b71302167529
CC諏訪 浩史 <h_suwa@cross-c.co.jp>
2020-04-13 18:45:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# concept
+The minimum selenium environment dedicated to writing scripts.
+
+# How to use
+1. `docker-compose up -d`
+2. `docker-compose exec app python {script}` ({script} must be python script on `app/` directory e.g. test.py)
---
+++ app/test.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+# from https://qiita.com/sikkim/items/447b72e6ec45849058cd
+from selenium import webdriver
+#from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from time import sleep
+import datetime
+import os
+
+# env setting
+host = 'selenium-chrome'
+wait_default = 1
+count_1 = 1
+
+# connection
+browser = webdriver.Remote(
+    command_executor='http://' + host + ':4444/wd/hub',
+    desired_capabilities=DesiredCapabilities.CHROME)
+
+# get datetime for filename
+dt = datetime.datetime.today()
+dtstr = dt.strftime("%Y%m%d%H%M%S")
+
+# open page
+browser.get('https://www.google.co.jp/')
+sleep(wait_default)
+
+# save screen shot image
+diretory = '/app/screenShot/' + dtstr + '/'
+if not os.path.exists(diretory):
+  os.makedirs(diretory)
+browser.save_screenshot(diretory + count_1 + '_' + browser.title + '.png')
+count_1 += 1
+
+# close
+browser.close()
+browser.quit()
