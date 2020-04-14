e3c7903c9e38e742bf6358b1a614be80b358184f
CC諏訪 浩史 <h_suwa@cross-c.co.jp>
2020-04-14 12:02:21 +0900

    ADD: setting to compile

--- node/scripts/entrypoint.sh
+++ node/scripts/entrypoint.sh
@@ -4,9 +4,5 @@ HOST_UID=1000
 
 NODE_MODULES_DIR=${WORKDIR_DOCKER}node_modules/
 
-if [ ! -e ${WORKDIR_DOCKER}package.json ]; then
-  echo 'INFO: Modules not installed.'
-  echo 'INFO: You must exec `npm init` and install selenium-webdriver typescript @types/selenium-webdriver.'
-  chown -R $HOST_UID $WORKDIR_DOCKER
-fi
+npm run watch
 bash
